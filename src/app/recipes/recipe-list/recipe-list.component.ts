7c6ec57dc5e19393db531e46e511f074d63bbfe0
Ignatius Seto <ignatius.seto@accenture.com>
2022-08-02 11:49:18 +0200

    finish course's section 3: course project - the basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Oven Roasted Crispy Pork Belly',
+      'This is simply a test',
+      'https://pinchandswirl.com/wp-content/uploads/2020/10/Crispy-Pork-Belly_square.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F31%2F16354-easy-meatloaf-mfs-74-1x1-1.jpg'
+    ),
+  ];
 
   constructor() {}
 
