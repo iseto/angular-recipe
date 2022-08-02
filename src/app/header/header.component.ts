2ab561052fd5a07d2458cd2217c12a656142190f
Ignatius Seto <ignatius.seto@accenture.com>
2022-08-02 18:39:19 +0200

    add navigation with event binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   // collapsed = true;
+
+  // create a new property which is an event emitter, and add Output decorator
+  // because we want to output data of this component to EXTERNAL component(s)
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
